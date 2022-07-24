8a391cb68223dfacd05d9d8f716da190a7d36ebf
BlutudlutMC <valecle3210@gmail.com>
2022-07-24 19:36:41 +0200

    Mainactyl

--- DAEMON/index.py
+++ DAEMON/index.py
@@ -1,9 +1,14 @@
 from flask import *
+import time
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    time.sleep(2)
     return 'There was an error while waiting for the server response. Please try again.'
-
+@app.errorhandler(404)
+def pagenotfound(e):
+    time.sleep(2)
+    return 'There was an error while waiting for the server response. Please try again.'
 if __name__ == '__main__':
     app.run(debug=True, port="8081", host="0.0.0.0")
