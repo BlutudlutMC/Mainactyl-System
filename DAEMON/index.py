e3281535e1b19bede39d4c6179e5751800d15590
BlutudlutMC <valecle3210@gmail.com>
2022-07-24 19:23:32 +0200

    Daemon Main

--- DAEMON/index.py
+++ DAEMON/index.py
@@ -1,1 +1,9 @@
-import docker
+from flask import *
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'There was an error while waiting for the server response. Please try again.'
+
+if __name__ == '__main__':
+    app.run(debug=True, port="8081", host="0.0.0.0")
