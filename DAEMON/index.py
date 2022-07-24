77b7cf46f75a598670c78e723bfeaf05e734bfc0
BlutudlutMC <valecle3210@gmail.com>
2022-07-24 19:16:24 +0200

    Added Daemon

--- DAEMON/index.py
+++
@@ -1,6 +0,0 @@
-import docker
-client = docker.DockerClient()
-
-bedrock = client.containers.run("marctv/minecraft-papermc-server:1.16", ports={'3000/tcp': 3000}, mem_limit="1g", working_dir="/root/blutudaemon/mcserver:/data:rw")
-for log in bedrock.logs(stream=True):
-    print(log)
